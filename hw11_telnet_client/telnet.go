f48b95a158b04820ce55c9aec0388ff798e77469
Victor Chirmicci <victorc@trendcapital.com>
2024-05-03 14:51:13 +0300

    HW11 is completed

--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"io"
+	"log"
+	"net"
 	"time"
 )
+
+const networkType = "tcp"
 
 type TelnetClient interface {
 	Connect() error
@@ -12,10 +16,49 @@ type TelnetClient interface {
 	Receive() error
 }
 
+type telnetClient struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	conn    net.Conn
+}
+
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	return &telnetClient{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+}
+
+func (t *telnetClient) Connect() error {
+	conn, err := net.DialTimeout(networkType, t.address, t.timeout)
+	if err != nil {
+		return err
+	}
+
+	t.conn = conn
+	log.Printf("Connected to %s", t.address)
+
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *telnetClient) Close() error {
+	if t.conn != nil {
+		err := t.conn.Close()
+		return err
+	}
+	return nil
+}
+
+func (t *telnetClient) Send() error {
+	_, err := io.Copy(t.conn, t.in)
+	return err
+}
+
+func (t *telnetClient) Receive() error {
+	_, err := io.Copy(t.out, t.conn)
+	return err
+}
